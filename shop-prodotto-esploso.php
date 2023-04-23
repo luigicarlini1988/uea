2740ab63e3bc30439e30c54873500656e7e11dd0
Luigi Carlini <luigicarlini88@gmail.com>
2023-04-23 12:16:04 +0530

    shop prodotto

---
+++ shop-prodotto-esploso.php
@@ -0,0 +1,187 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Uomo Ambiente | Shop Prodotto</title>
+
+    <!-- fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap"
+        rel="stylesheet">
+
+    <!--CSS -->
+    <link rel="stylesheet" href="css/general.css" />
+    <link rel="stylesheet" href="css/responsive.css" />
+
+    <!-- CSS required when there are carousels-->
+    <link rel="stylesheet" href="css/owl.carousel.min.css" />
+    <link rel="stylesheet" href="css/owl.theme.default.min.css" />
+
+
+    <!--JS / Jquery -->
+    <script type="text/javascript" src="js/jquery-3-10-1-min.js"></script>
+    <script type="text/javascript" src="js/general-animations.js"></script>
+
+
+
+</head>
+
+<body class="woocommerce">
+    <?php
+        include ('incl/top-menu.html');
+
+     ?>
+
+    <?php
+        include ('incl/top-header-prodotto-esploso.html');
+
+     ?>
+
+    <section id="prodotto-page-content">
+        <div class="foglio flex justify-evenly">
+            <div class="col-4">
+                <div class="sidecar duration flex justify-left">
+                    <p class="white bold">Duration:</p>
+                    <p class="white"><i>16 ore</i></p>
+                </div>
+
+                <div class="sidecar dove flex justify-left">
+                    <p class="dark-blu bold">Dove</p>
+                    <div class="side-bottom">
+                        <p class="lh2"><i>Il corso è attivabile su <a href="#">richiesta</a> presso la sede del
+                                cliente e presso le nostre sedi</i></p>
+                    </div>
+                </div>
+
+                <div class="sidecar flex justify-left">
+                    <p class="dark-blu bold">Corso di:</p>
+                    <p class=""><i>base</i></p>
+                </div>
+
+                <div class="sidecar flex justify-left">
+                    <p class="dark-blu bold">Corso per</p>
+                    <div class="side-bottom">
+                        <p class="lh2"><i>Categoria Lavorativa 1</i></p>
+                        <p class="lh2"><i>Categoria Lavorativa 2</i></p>
+                        <p class="lh2"><i>Categoria Lavorativa 3</i></p>
+                    </div>
+                </div>
+
+                <div class="sidecar flex justify-left">
+                    <p class="dark-blu bold">Codice Corso:</p>
+                    <p class=""><i>GHB5451</i></p>
+                </div>
+
+            </div>
+            <div class="col-8">
+                <div class="small-content-detail">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                        labore
+                        et
+                        dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                        aliquip
+                        ex ea commodo consequat other two (240)</p>
+                    <p class="dark-blu titles5"><i>Il Corso è al momento in programma modalità webinar</i></p>
+                </div>
+                <div class="plug-in-wizard-wrap">
+                    <div class="plu">
+                        <p>place plugin stuff here</p>
+                    </div>
+
+                    <div class="nota-bene">
+                        <p class="dark-blu blod titles5">Not bene</p>
+                        <p class="dark-blu"><i>Il Corso verrà erogato al raggiungimento del numero minimo di
+                                partecipanti</i></p>
+                    </div>
+                    <div class="detail-product-cta">
+                        <?php 
+                            include('incl/card-cta-shop-white.html') ;
+                        ?>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+
+    <?php
+        include ('incl/divider-prodotto-contatti.html');
+
+     ?>
+
+    <section id="prodotto-page-content" class="second-section">
+        <div class="foglio flex justify-evenly">
+            <div class="col-4">
+                <div class="additional-side">
+                    <p class="bold dark-blu">Verifica Finale</p>
+                    <p><i>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                            labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation </i></p>
+                </div>
+
+                <div class="additional-side">
+                    <p class="bold dark-blu">Other thing</p>
+                    <p><i>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                            labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation </i></p>
+                </div>
+
+                <div class="additional-side">
+                    <p class="bold dark-blu">Other thing</p>
+                    <p><i>Ut enim ad minim veniam, quis nostrud exercitation </i></p>
+                </div>
+
+                <div class="additional-side">
+                    <p class="bold dark-blu">Other thing</p>
+                    <p><i>Tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+                            exercitation </i></p>
+                </div>
+            </div>
+            <div class="col-8">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+                    et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                    aliquip ex ea commodo consequat. <br /><br />Duis aute irure dolor in reprehenderit in voluptate
+                    velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                    sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+                <div class="video-wrap">
+                    <iframe src="https://www.youtube.com/embed/Ti6qhk3tX2s" frameborder="0"
+                        allow="accelerometer; autoplay; encrypted-media; gyroscope;" allowfullscreen></iframe>
+                </div>
+
+                <p class="titles4 dark-blu bold">Programma:</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+                    et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                    aliquip ex ea commodo consequat. <br /><br />Duis aute irure dolor in reprehenderit in voluptate
+                    velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                    sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+                <div class="detail-product-cta">
+                    <?php 
+                            include('incl/card-cta-shop-white.html') ;
+                        ?>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <?php 
+        include('incl/portlet-prodotti.html') ;
+    ?>
+
+    <?php 
+        include('incl/form-esploso.html') ;
+    ?>
+
+    <?php 
+        include('incl/footer.html') ;
+    ?>
+
+
+</body>
+
+</html>
