76eed2e27faf48f12aa3c5ca3b9464b1924e38c8
Luigi Carlini <luigicarlini88@gmail.com>
2023-05-27 12:22:39 +0530

    template metodo

---
+++ corporate-metodo.php
@@ -0,0 +1,181 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Uomo Ambiente | Metodo</title>
+
+    <!-- fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap"
+        rel="stylesheet">
+
+    <!--CSS -->
+    <link rel="stylesheet" href="css/general.css" />
+    <link rel="stylesheet" href="css/responsive.css" />
+
+
+
+    <!--JS / Jquery -->
+    <script type="text/javascript" src="js/jquery-3-10-1-min.js"></script>
+    <script type="text/javascript" src="js/general-animations.js"></script>
+
+
+
+</head>
+
+<body>
+    <?php
+        include ('incl/top-menu.html');
+
+     ?>
+
+    <?php
+        include ('incl/top-header-supersimple.html');
+
+     ?>
+
+    <section id="opening-corp">
+        <div class="foglio flex justify-evenly align-center">
+            <div class="col-7">
+                <p class="titles2 dark-blu">La sostenibilità è una cosa semplice. Se autentica</p>
+                <p>Possiamo rendere la tua impresa più sostenibile. Lo faremo con l’esperienza di chi da 20 anni è
+                    esempio di progresso sostenibile e crede nella competenza e nelle persone come valore. Lo faremo
+                    perché siamo convinti che la sostenibilità sia un valore innato dentro ogni essere umano.</p>
+            </div>
+            <div class="col-5">
+                <img class="opening-img" src="img/onpage/home-opening.jpeg" />
+            </div>
+        </div>
+    </section>
+
+
+    <section class="metodo-1">
+        <div class="foglio flex">
+            <div class="col-8">
+                <div class="met-row">
+                    <p class="titles3 dark-blu">I nostri servizi, perle della collana della sostenibilità</p>
+                    <p>In UOMOeAMBIENTE crediamo che ogni servizio erogato sia parte di un percorso più ampio di
+                        consapevolezza personale e aziendale verso la sostenibilità. Una sostenibilità innata nell’uomo,
+                        ma non istintiva e per questo da coltivare per farla diventare valore.
+                        Sì, la sostenibilità per noi è come quel filo immaginario che unisce tutti i nostri servizi,
+                        restituendo una collana, il cui valore è dato dalla visione della sua interezza, dall’insieme
+                        delle perle. Perché tutto è sostenibilità e la sostenibilità è tutto.
+                        Che si tratti di sicurezza sul lavoro o progetti ambientali, consulenza organizzativa o del
+                        calcolo della carbon footprint, non mancheremo mai di valorizzare questi interventi come perle
+                        di questa preziosissima collana.
+                    </p>
+                </div>
+                <div class="met-row">
+                    <p class="titles3 dark-blu">Il valore dell'esempio</p>
+                    <p>Alla base del nostro approccio olistico alla sostenibilità, vi è la voglia di far emergere
+                        l’autenticità delle organizzazioni e quindi delle persone che in esse operano. Essere autentici
+                        significa essere ciò che si dice di essere, credere in ciò in cui si dice di credere, fare ciò
+                        che si dice di saper fare.
+                        La sostenibilità di UOMOeAMBIENTE ha origine nella sua autenticità. E così, da sempre, ciò che
+                        proponiamo ai nostri clienti lo abbiamo già sperimentato con successo prima su noi stessi e sul
+                        nostro modello organizzativo.
+                        Questo è UOMOeAMBIENTE, questo è il nostro metodo.
+                    </p>
+                </div>
+                <div class="met-row">
+                    <p class="titles3 dark-blu">Sostenibilità su misura</p>
+                    <p>Vogliamo che i servizi e le consulenze che eroghiamo siano modellati singolarmente per ogni
+                        nostro cliente.
+                        Perché un percorso di consulenza sia efficace, serve che sia pensato, progettato e attuato a
+                        misura di ogni cliente. In UOMOeAMBIENTE facciamo proprio questo. Perché a vestire abiti fuori
+                        taglia o quelli di qualcun altro non ci si può sentire sé stessi.
+                        Siamo i sarti della vostra sostenibilità: quelli che ti prendono le misure e te la cuciono
+                        addosso, come una seconda pelle. Autenticamente tua.
+
+                    </p>
+                </div>
+            </div>
+
+            <div class="col-4 flex align-center justify-right">
+                <img class="gen-ic" src="img/icons/trigloo-icons/generic.svg" />
+            </div>
+        </div>
+    </section>
+
+    <section id="c-passi">
+        <div class="foglio">
+            <p class="titles2 dark-blu">5 passi per un successo sostenibile</p>
+            <div class="passi-section">
+                <p class="titles3 dark-blu">1 - Progettazione del servizio</p>
+                <p>Metodi operativi e strumenti per l’innovazione e la sostenibilità rappresentano parti essenziali
+                    della proposta progettuale così come la determinazione dei benefici effettivi derivanti dal progetto
+                    e la ricerca di incentivi e strumenti di finanza e formazione agevolata a supporto
+                    dell’investimento.</p>
+            </div>
+
+            <div class="passi-section">
+                <p class="titles3 dark-blu">2 - Pianificazione delle attività</p>
+                <p>UOMOeAMBIENTE si avvale di un avanzato sistema gestionale informatico per pianificare, organizzare e
+                    facilitare i flussi di lavoro dei consulenti e per gestire in modo dinamico e reattivo ogni
+                    variazione di programma concordata con il cliente.</p>
+            </div>
+
+            <div class="passi-section">
+                <p class="titles3 dark-blu">3 - Designazione della squadra di lavoro</p>
+                <p>Per ogni commessa viene designato un project manager che si interfaccerà con il cliente in modo
+                    esclusivo. Sotto il suo coordinamento opera il team di progetto, i cui componenti sono individuati
+                    in base alle competenze richieste da ogni commessa. </p>
+            </div>
+
+            <div class="passi-section">
+                <p class="titles3 dark-blu">4 - Erogazione del servizio di monitoraggio</p>
+                <p>UOMOeAMBIENTE assicura un costante monitoraggio sull’avanzamento di tutte le commesse, a cura della
+                    sua direzione tecnica, assicura il rispetto delle tempistiche contrattuali e la valutazione
+                    sull’efficacia dell’intervento in corso di svolgimento.
+                </p>
+            </div>
+
+            <div class="passi-section">
+                <p class="titles3 dark-blu">5. Conclusione del servizio di monitoraggio</p>
+                <p>Per UOMOeAMBIENTE la commessa è da ritenersi conclusa solo quando i risultati ottenuti sono coerenti
+                    con gli obiettivi prestabiliti. A fine progetto UeA incontra il cliente per condividere i risultati
+                    e valutarne l’effettiva soddisfazione perché riteniamo che il cliente soddisfatto, grazie al
+                    passaparola, sia il nostro più importante canale di promozione.
+                </p>
+            </div>
+        </div>
+    </section>
+
+
+
+
+
+
+
+    <?php 
+        include('incl/sostenibilita.html') ;
+    ?>
+
+    <?php 
+        include('incl/ambiti-intervento-white.html') ;
+    ?>
+
+    <?php 
+        include('incl/nostri-servizi.html') ;
+    ?>
+
+
+
+    <?php 
+        include('incl/newsletter.html') ;
+    ?>
+
+
+    <?php 
+        include('incl/footer.html') ;
+    ?>
+
+
+</body>
+
+</html>
